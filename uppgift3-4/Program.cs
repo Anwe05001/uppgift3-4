54a65f1559b6fdba32640853982b840b5d721016
Anwe05001 <144337047+Anwe05001@users.noreply.github.com>
2023-10-27 12:49:16 +0200

    Update Program.cs

--- uppgift3-4/Program.cs
+++ uppgift3-4/Program.cs
@@ -1,33 +1,35 @@
 ﻿using System;
-using System.ComponentModel.Design;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hur lång är låten du vill sätta in?");
-        Console.WriteLine("skriv först minuter");
+        Console.WriteLine("Skriv först minuter");
 
-        int minuter = int.Parse (Console.ReadLine());
+        int minuter = int.Parse(Console.ReadLine());
 
         Console.WriteLine("Skriv nu i sekunder");
 
         int sekunder = int.Parse(Console.ReadLine());
 
-        if (minuter < 2 || minuter <= 2 && sekunder < 45)
-        {
-            Console.WriteLine("Denna låt är tyvärr ej lång nog");
-        }
-
-        if (minuter > 4 || minuter >= 4 && sekunder > 20)
-        {
-            Console.WriteLine("Denna låt är tyvärr för lång");
-        }
-
-        else
+        if ((minuter == 2 && sekunder >= 45) || (minuter > 2 && minuter < 4) || (minuter == 4 && sekunder <= 20))
         {
             Console.WriteLine("Denna låten blir perfekt!");
         }
+        else if (minuter < 2 || (minuter == 2 && sekunder < 45))
+        {
+            Console.WriteLine("Denna låt är tyvärr ej lång nog");
+        }
+        else if (minuter > 4 || (minuter == 4 && sekunder > 20))
+        {
+            Console.WriteLine("Denna låt är tyvärr för lång");
+        }
+        else
+        {
+            Console.WriteLine("Denna låt ligger inte inom det tillåtna intervallet.");
+        }
+
         Console.ReadKey();
     }
 }
